7652f7e0b3185174031378d0f475dad754e47e35
colozz <colozz@Paarthurnax>
2018-04-06 18:03:47 +0200

    Rework

--- HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/HelloGitHub.java
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/HelloGitHub.java
@@ -3,16 +3,6 @@ package fr.imta.prime.cacao2018.hellogithub;
 public class HelloGitHub {
 
 	public static void main(String[] args) {
-		hello();
-	}
-
-	public static void hello() {
-		System.out.println("Thomas Schillaci");
-		System.out.println("Maxim Poulsen");
-
-		System.out.println("Juliette Gorline");
-
-		System.out.println("François Le Guernic ");
 		
 	}
 	
