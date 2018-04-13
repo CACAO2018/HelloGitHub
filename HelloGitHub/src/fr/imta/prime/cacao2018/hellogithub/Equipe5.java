9c99d383a7fad6a3d16307a5f9397dc00787a6ed
colozz <colozz@Paarthurnax>
2018-04-13 14:23:07 +0200

    Update

--- HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/Equipe5.java
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/Equipe5.java
@@ -2,6 +2,10 @@ package fr.imta.prime.cacao2018.hellogithub;
 
 public class Equipe5 {
 	
+	public static void main(String[] args) {
+		hello();
+	}
+	
 	public static void hello() {
 		System.out.println("Thomas Schillaci");
 		System.out.println("Maxim Poulsen");
